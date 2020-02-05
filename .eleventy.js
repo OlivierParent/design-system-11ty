a76f22b88c3ace4052be1a4ae1d9302b314d1725
OlivierParent <olivier.parent@arteveldehs.be>
2020-02-05 22:56:32 +0100

    [WIP]

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({
+    "css": "assets/css",
+    "images": "assets/images"
+  });
+
+  return {
+    dir: {
+      input: "views",
+      output: "docs"
+    },
+    pathPrefix: "/design-system/"
+  };
+};
