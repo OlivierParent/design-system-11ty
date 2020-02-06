04f610dc1b113a2766eb15e6501a93970273e7fb
OlivierParent <olivier.parent@arteveldehs.be>
2020-02-06 19:37:04 +0100

    [WIP]

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,19 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({
     "css": "assets/css",
     "images": "assets/images"
   });
 
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
   return {
     dir: {
       input: "views",
       output: "docs"
     },
+    htmlTemplateEngine: "njk",
     pathPrefix: "/design-system-11ty/"
   };
 };
